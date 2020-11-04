1d79a53f5bd28f63eb63325973e1bff45429353a
Harshit Raj <harshitraj712@gmail.com>
2020-11-04 12:40:18 +0530

    SQL db added

--- express.js
+++ express.js
@@ -1,24 +1,75 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
+const bodyparser = require('body-parser');
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false}));
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "NewDatabase"
+})
 app.get('/',(req,res) =>{
   res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/adduser',(req,res)=>{
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var regNumber = req.body.regNumber;
 
 
-app.post('/read',(req,res)=>{
-  res.send('This is read!')
+  var query = 'INSERT INTO userinfo (firstName,lastName,regNumber) VALUES ("'+firstName+'","'+lastName+'", "'+regNumber+'")';
+  con.query(query,(err,results)=>{
+    if(err) {res.send(err.message);}
+    else{ res.send({
+      status:200,
+      message:"data was entered"
+    })}
+  })
 })
 
-app.put('/update',(req,res)=>{
-  res.send('This is update!')
-})
+app.post("/update", (req, res) => {
+  let regNumber = req.body.regNumber;
+  let newFirst = req.body.newFirst;
+  let newLast = req.body.newLast;
 
-app.delete('/delete',(req,res)=>{
-  res.send('This is Delete!!')
-})
+  let query = `UPDATE userinfo SET firstName = "${newFirst}" , lastName = "${newLast}" WHERE regNumber = ${regNumber} `;
+res.send({
+  con.query(query, (err, result) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send({
+        status: 200,
+        message: "Data is Updated.",
+      });
+    }
+  });
+});
+
+app.post("/delete",(req,res)=>{
+  let regNumber = req.body.regNumber;
+
+  let query = `DELETE FROM userinfo WHERE  regNumber = ${regNumber} `;
+
+  con.query(query, (err, result) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send({
+        status: 200,
+        message: "Data was Deleted.",
+      });
+    }
+  });
+
+});
+
+
+
 
 app.listen(4000,() => console.log('Server is running'))
